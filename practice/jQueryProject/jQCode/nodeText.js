729fb8a801fd3324e44226f25a458f0a4accbd1b
Hubert-bor <3306909324@qq.com>
2021-11-22 11:57:40 +0800

    update 2021/11/22 11:57

---
+++ practice/jQueryProject/jQCode/nodeText.js
@@ -0,0 +1,2 @@
+console.log("node.js statr time's 2021/11/21 19:15");
+
---
+++ practice/jQueryProject/serverCode/server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+http.createServer(function(ruquest,response){
+    //发送HTTP头部
+    //HTTP 状态值 ：200：OK
+    //内容类型：text/plain
+    response.writeHead(200,{'Context-Type':'text/plain'});
+
+    //发送响应数据"Hello World"
+    response.end('Hello World\n')
+}).listen(8888);
+
+//终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
